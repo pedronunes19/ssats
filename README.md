c4932170de7986033be38dc2e579a905f746d3c7
pedronunes19 <pedromiguelmnunes@gmail.com>
2025-03-21 21:53:16 +0000

    unit propagation

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # ssats
 
 Program to determine whether a given Boolean formula, represented in Conjunctive Normal Form, can be satisfied.
+
+### DPLL (Davis-Putnam-Logemann-Loveland) Algorithm
+
+- [x] Unit Propagation
+- [ ] Pure Literal Elimination
+- [ ] Recursive Splitting
--- main.hs
+++ main.hs
@@ -2,13 +2,13 @@
 -- I'm too lazy to implement a parser but it can just be done on top of this
 -- e.g. (a OR b) AND (¬a OR c) <==> [[1, 2], [-1, 3]]
 
-import Data.List (nub)
+import Data.List (nub, partition)
 import Data.Set (fromList, intersection, empty)
 
 type Literal = Int  -- Positive for x, Negative for ¬x
 type Clause = [Literal]  -- A clause is a list of literals
 type CNFF = [Clause]  -- A CNF formula is a list of clauses
-type Assignment = [(Int, Bool)]  -- store variable assignment
+type Assignment = [(Literal, Bool)]  -- store variable assignment
 
 
 -- function to remove duplicate literals 
@@ -25,7 +25,33 @@ removeTautologies :: CNFF -> CNFF
 removeTautologies = filter (not . isTautology)
 
 
+-- function to check for unit clauses
+findUnitClause :: CNFF -> Maybe Literal
+findUnitClause [] = Nothing
+findUnitClause (c:cs)
+    | length c == 1 = Just (head c) -- return literal in unit clause
+    | otherwise     = findUnitClause cs 
+
+-- function to trim formula after finding unit clause
+simplifyCNFF :: CNFF -> Literal -> CNFF
+simplifyCNFF cnff lit =
+    let (satisfied, remaining) = partition (elem lit) cnf -- get satisfied clauses
+    in map (filter (/= -lit)) remaining  -- remove ¬lit from remaining clauses
+
+unitPropagation :: CNFF -> Assignment -> (CNFF, Assignment)
+unitPropagation cnff assignment =
+    case findUnitClause cnff of
+        Nothing     -> (cnff, assignment)  -- done
+        Just lit    -> 
+            let newCNFF = simplifyCNFF cnff lit
+                newAssignment = lit : assignment
+            in unitPropagation newCNFF newAssignment  -- recursively propagate
+
+
 
 -- Example
 cnfFormula :: CNF
 cnfFormula = [[1, 2], [-1, 3], [-2, -3]]
+
+assignment :: Assignment
+assignment = []
